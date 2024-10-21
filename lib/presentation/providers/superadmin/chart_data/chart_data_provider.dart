35f48ef86bf5d2ff247c2ae02e0b2f841ae7051c
Vunky Himawan <tingkerbeii44@gmail.com>
2024-10-22 06:57:57 +0700

    feat: add super admin home page

--- lib/presentation/providers/superadmin/chart_data/chart_data_provider.dart
+++ lib/presentation/providers/superadmin/chart_data/chart_data_provider.dart
@@ -1,8 +1,8 @@
 import 'package:eventpass_app/domain/entities/chart_data.dart';
 import 'package:eventpass_app/presentation/misc/colors.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-final revenueData = Provider((ref) => [
+final countNewRegisteredUsers = Provider((ref) => [
       ChartData('Jan', 100),
       ChartData('Feb', 1100),
       ChartData('Mar', 300),
@@ -17,7 +17,7 @@ final revenueData = Provider((ref) => [
       ChartData('Dec', 1200),
     ]);
 
-final ticketSalesData = Provider((ref) => [
+final countUploadedEvents = Provider((ref) => [
       ChartData('Jan', 100),
       ChartData('Feb', 1100),
       ChartData('Mar', 300),
@@ -32,13 +32,28 @@ final ticketSalesData = Provider((ref) => [
       ChartData('Dec', 1200),
     ]);
 
-final categorySalesData = Provider((ref) => [
+final countTicketSales = Provider((ref) => [
+      ChartData('Jan', 100),
+      ChartData('Feb', 1100),
+      ChartData('Mar', 300),
+      ChartData('Apr', 1500),
+      ChartData('May', 500),
+      ChartData('Jun', 2000),
+      ChartData('Jul', 700),
+      ChartData('Aug', 800),
+      ChartData('Sep', 900),
+      ChartData('Oct', 1000),
+      ChartData('Nov', 1100),
+      ChartData('Dec', 1200),
+    ]);
+
+final categoryGroupSales = Provider((ref) => [
       ChartData('Seminar', 100),
       ChartData('Conference', 1100),
       ChartData('Workshop', 300),
     ]);
 
-final ageGroupData = Provider((ref) => [
+final ageGroupSales = Provider((ref) => [
       ChartData('18-24', 100),
       ChartData('25-34', 1100),
       ChartData('35-44', 300),
@@ -49,7 +64,7 @@ final ageGroupData = Provider((ref) => [
       ChartData('85+', 800),
     ]);
 
-final genderData = Provider((ref) => [
+final genderSales = Provider((ref) => [
       ChartData('Laki-laki', 100, softPink),
       ChartData('Perempuan', 1100, softPurple),
     ]);
