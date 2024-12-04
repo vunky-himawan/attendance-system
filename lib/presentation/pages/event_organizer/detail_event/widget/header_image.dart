dd0fc490d9f920b86fe4363b613715f93c944198
Haidarly2 <haidar.aly007@gmail.com>
2024-12-04 09:25:27 +0700

    commit add new widget for detail event EO

---
+++ lib/presentation/pages/event_organizer/detail_event/widget/header_image.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class HeaderImage extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final VoidCallback onBackPress;
+
+  const HeaderImage({
+    required this.imageUrl,
+    required this.title,
+    required this.onBackPress,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.network(
+          imageUrl,
+          width: double.infinity,
+          height: 250,
+          fit: BoxFit.cover,
+        ),
+        Positioned(
+          top: 16,
+          left: 16,
+          child: IconButton(
+            onPressed: onBackPress,
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+          ),
+        ),
+        Positioned(
+          top: 16,
+          right: 16,
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
