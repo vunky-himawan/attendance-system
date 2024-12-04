e84fdfbc16dffd9a599bde4de7193aa5187be764
Haidarly2 <haidar.aly007@gmail.com>
2024-12-04 09:42:05 +0700

    commit add new widget for detail event

---
+++ lib/presentation/pages/event_organizer/detail_event/widget/statistic_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class StatisticItem extends StatelessWidget {
+  final String title;
+  final String value;
+
+  const StatisticItem({
+    required this.title,
+    required this.value,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            value,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
