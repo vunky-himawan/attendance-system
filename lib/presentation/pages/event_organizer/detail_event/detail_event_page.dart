36f4534cfdca88bb2134603535a286eed31b1639
Haidarly2 <haidar.aly007@gmail.com>
2024-12-04 22:16:15 +0700

    commit changes detail event EO

--- lib/presentation/pages/event_organizer/detail_event/detail_event_page.dart
+++ lib/presentation/pages/event_organizer/detail_event/detail_event_page.dart
@@ -2,6 +2,8 @@ import 'package:eventpass_app/presentation/pages/event_organizer/detail_event/wi
 import 'package:eventpass_app/presentation/pages/event_organizer/detail_event/widget/speaker_card.dart';
 import 'package:eventpass_app/presentation/pages/event_organizer/detail_event/widget/statistic_item.dart';
 import 'package:eventpass_app/presentation/pages/event_organizer/detail_event/widget/tab_navigation.dart';
+import 'package:eventpass_app/presentation/providers/admin/chart_data/chart_data_provider.dart';
+import 'package:eventpass_app/presentation/widgets/chart/bar_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -41,7 +43,7 @@ class DetailEventTabPage extends ConsumerWidget {
             children: [
               // Header Image
               HeaderImage(
-                imageUrl: 'https://via.placeholder.com/400x250',
+                imageUrl: 'https://images.pexels.com/photos/321552/pexels-photo-321552.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1/400x250',
                 title: 'Detail Acara',
                 onBackPress: () => Navigator.pop(context),
               ),
@@ -56,7 +58,7 @@ class DetailEventTabPage extends ConsumerWidget {
               ),
 
               // Tab Content
-              selectedIndex == 0 ? _buildDetailSection() : _buildStatistikSection(),
+              selectedIndex == 0 ? _buildDetailSection() : _buildStatistikSection(context, ref),
             ],
           ),
         ),
@@ -122,27 +124,22 @@ class DetailEventTabPage extends ConsumerWidget {
   }
 
   // Statistik Section
-  Widget _buildStatistikSection() {
+  Widget _buildStatistikSection(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const StatisticItem(title: 'Tiket Terjual', value: '400/400'),
-          const StatisticItem(title: 'Pengunjung', value: '400'),
-          const StatisticItem(title: 'Absensi Masuk', value: '400'),
-          const StatisticItem(title: 'Absensi Keluar', value: '400'),
-          const StatisticItem(title: 'Pendapatan', value: 'Rp. 30.000.000'),
+          const StatisticItem(title: 'Tiket Terjual', subtitle: 'Total tiket yang terjual', value: '400/400'),
+          const StatisticItem(title: 'Pengunjung', subtitle: 'Pengunjung yang terdaftar',value: '400'),
+          const StatisticItem(title: 'Absensi Masuk', subtitle: 'Pengunjung yang melakukan absensi masuk',value: '400'),
+          const StatisticItem(title: 'Absensi Keluar', subtitle: 'Pengunjung yang melakukan absensi keluar',value: '400'),
+          const StatisticItem(title: 'Pendapatan', subtitle: 'Total pemasukan dari penjualan tiket',value: 'Rp. 30.000.000'),
           const SizedBox(height: 24),
-          const Text(
-            'Kehadiran',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 8),
           Container(
-            height: 200,
-            color: Colors.grey.shade300, // Placeholder for chart
-            child: const Center(child: Text('Chart Placeholder')),
+            height: 350,
+            color: Colors.white, // Placeholder for chart
+            child: barChart(context, ref, 'Kehadiran', ref.watch(ageGroupData))
           ),
         ],
       ),
